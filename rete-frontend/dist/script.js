a1731bc73857cfee9adb36153c5d8b37bf0dc857
tomerbit <bitrant@post.bgu.ac.il>
2021-02-16 13:52:53 +0200

    Make node smaller

--- rete-frontend/dist/script.js
+++ rete-frontend/dist/script.js
@@ -141,19 +141,22 @@ var CustomBSyncNode = {
     <div class="title">{{node.name}}</div>
     <!-- Outputs-->
     <div class="output" v-for="output in outputs()" :key="output.key">
-      <div class="output-title">{{output.name}}</div>
+    <!-- <div class="output-title">{{output.name}}</div> -->
       <Socket v-socket:output="output" type="output" :socket="output.socket"></Socket>
     </div>
+
+    <!-- Inputs-->
+    <div class="input" v-for="input in inputs()" :key="input.key">
+      <Socket v-socket:input="input" type="input" :socket="input.socket"></Socket>
+      <!-- <div class="input-title" v-show="!input.showControl()">{{input.name}}</div> -->
+      <div class="input-control" v-show="input.showControl()" v-control="input.control"></div>
+    </div>
+    
     <!-- Controls-->
     <div class="control" v-for="control in controls()" v-control="control">
             <div class="control-title">{{control.key}}:</div>
     </div>
-    <!-- Inputs-->
-    <div class="input" v-for="input in inputs()" :key="input.key">
-      <Socket v-socket:input="input" type="input" :socket="input.socket"></Socket>
-      <div class="input-title" v-show="!input.showControl()">{{input.name}}</div>
-      <div class="input-control" v-show="input.showControl()" v-control="input.control"></div>
-    </div>
+
   </div>`,
     mixins: [VueRenderPlugin.mixin],
     components: {
